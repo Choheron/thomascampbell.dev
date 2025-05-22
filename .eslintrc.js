fd75ffaeaef67492d1f80d80aeca6b4e0954afe7
Thomas Campbell <trc527@gmail.com>
2025-05-22 16:51:36 -0500

    Again

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    "@typescript-eslint/no-explicit-any": "off"
-  }
-};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", {
+      extends: ['next'],
+      rules: {
+        'react/no-unescaped-entities': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off'
+      },
+    }
+  ),
 ];
 
 export default eslintConfig;
