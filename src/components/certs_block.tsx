a08ba6e163921b4f7f60a36ec5447ddf4ba5cd6a
Thomas Campbell <trc527@gmail.com>
2025-05-22 18:39:48 -0500

    Css fix

--- src/components/certs_block.tsx
+++ src/components/certs_block.tsx
@@ -3,7 +3,7 @@ export default function CertsBlock() {
   return (
     <div className="w-full h-fit px-10 py-2 max-w-full">
       <p className="text-3xl underline pb-2"><b>Certifications:</b></p>
-      <div className="w-full mx-auto flex justify-around py-10">
+      <div className="w-full mx-auto flex flex-wrap justify-around py-10">
         <a href="https://www.credly.com/badges/fd660734-8fa0-4b83-888c-fa516e14c5db/public_url" target="_blank" rel="noreferrer noopener" title="AWS Certified Solutions Architect Associate">
           <img className="size-50 hover:scale-110 transition duration-200" src={`/images/certs/AWS_ASA_BADGE.png`} alt="AWS Certified Solutions Architect Associate Badge" />
         </a>
