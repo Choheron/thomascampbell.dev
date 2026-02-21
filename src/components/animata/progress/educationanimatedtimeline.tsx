f1ab19655b59191af360fb399cc6dca4b837b02a
Thomas Campbell <trc527@gmail.com>
2026-02-20 19:53:07 -0600

    Fix css

--- src/components/animata/progress/educationanimatedtimeline.tsx
+++ src/components/animata/progress/educationanimatedtimeline.tsx
@@ -97,23 +97,21 @@ const EduTimelineItem: React.FC<EduTimelineItemProps> = ({
           customRender(event)
         ) : (
           <>
-            <div className="flex flex-col sm:flex-row justify-between -mt-1">
-              <h3 className="text-2xl font-semibold" style={{ color: styles.titleColor }}>
-                {event.school}
-              </h3>
-              <span className="text-base mb-auto" style={{ color: styles.dateColor }}>
-                {event.date}
-              </span>
-            </div>
+            <p className="text-xl font-semibold" style={{ color: styles.titleColor }}>
+              {event.school}
+            </p>
+            <span className="text-sm mb-auto" style={{ color: styles.dateColor }}>
+              {event.date}
+            </span>
             <div className="flex gap-1 my-1">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-black dark:fill-white size-[1.3rem]"><path d="M18.364 17.364L12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364ZM12 13C13.1046 13 14 12.1046 14 11C14 9.89543 13.1046 9 12 9C10.8954 9 10 9.89543 10 11C10 12.1046 10.8954 13 12 13Z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-black dark:fill-white size-[1rem]"><path d="M18.364 17.364L12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364ZM12 13C13.1046 13 14 12.1046 14 11C14 9.89543 13.1046 9 12 9C10.8954 9 10 9.89543 10 11C10 12.1046 10.8954 13 12 13Z"></path></svg>
               <p style={{ color: styles.descriptionColor }} className="my-auto">{event.location}</p>
             </div>
-            <h3 className="text-xl" style={{ color: styles.titleColor }}>
+            <h3 className="text-base" style={{ color: styles.titleColor }}>
               {event.degree}
             </h3>
             {event.subdegree && 
-              <h3 className="text-lg" style={{ color: styles.titleColor }}>
+              <h3 className="text-sm" style={{ color: styles.titleColor }}>
                 {event.subdegree}
               </h3>
             }
